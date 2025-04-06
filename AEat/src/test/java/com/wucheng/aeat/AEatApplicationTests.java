4289b5bbf853dcf3dc1adac2713472345bf25ef8
Polar-Day-Dream <wc17398181715@outlook.com>
2025-04-06 20:35:39 +0800

    sout0000201

--- AEat/src/test/java/com/wucheng/aeat/AEatApplicationTests.java
+++ AEat/src/test/java/com/wucheng/aeat/AEatApplicationTests.java
@@ -24,7 +24,7 @@ class AEatApplicationTests {
     @Test
     void test2(){
         System.out.println("测试git分支");
-        System.out.println("000000000002222");
+        System.out.println("00000000000222");
     }
 
 
